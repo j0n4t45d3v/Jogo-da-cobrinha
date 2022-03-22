d95fdbed4ac4a550fa6dd9a91ddf661f9eb90a29
j0n4t45d3v <delimajonatas875@gmail.com>
2022-03-22 10:06:56 -0300

    cobrinha

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById('cobra')
 let contexto = canvas.getContext('2d')
 let box = 32
+let cobra = []
+cobra[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     contexto.fillStyle = 'lightgreen'
     contexto.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+function criarCobrinha(){
+    for(i = 0; i < cobra.length; i++){
+        contexto.fillStyle = 'green'
+        contexto.fillRect(cobra[i].x, cobra[i].y, box, box)
+    }
+}
+
 criarBG()
+criarCobrinha()
